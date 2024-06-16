bf3a4bc31faa8c3af94837f7d6fa3f96b24bc75e
von Schappler <37213588+vonschappler@users.noreply.github.com>
2024-06-16 05:04:57 -0300

    Add base template for cra-no-tailwind

---
+++ cra-no-tailwind/src/components/Copyright.jsx
@@ -0,0 +1,12 @@
+const Copyright = () => {
+  return (
+    <div className='copyright'>
+      <p>Template created by von Schappler</p>
+      <span>
+        All rights reserved &bull; (2010 - {new Date().getFullYear()})
+      </span>
+    </div>
+  );
+};
+
+export default Copyright;
